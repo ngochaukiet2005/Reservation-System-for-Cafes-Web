b6558261152bd06fdf844d5e38d8b183d391e3b7
AuNguyen365 <aunguyen435@gmail.com>
2025-12-19 00:52:41 +0700

    kết nối lại cơ sở dữ liệu

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+console.log('CWD =', process.cwd());
+console.log('DB_PASSWORD =', process.env.DB_PASSWORD);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
